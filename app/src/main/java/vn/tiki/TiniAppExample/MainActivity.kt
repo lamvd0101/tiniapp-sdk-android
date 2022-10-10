eb56855506eb4c3f71366aa2983534726148fd6b
Liam Vuong <lamvd0101@gmail.com>
2022-10-10 17:02:07 +0700

    Sync

--- app/src/main/java/vn/tiki/TiniAppExample/MainActivity.kt
+++ app/src/main/java/vn/tiki/TiniAppExample/MainActivity.kt
@@ -1,11 +1,8 @@
 package vn.tiki.TiniAppExample
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+class MainActivity : Activity() {
+
 }
