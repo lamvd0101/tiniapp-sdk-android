3ca21cdb6f5a54ca29ad33ea32b089ee51e3a870
Liam Vuong <lamvd0101@gmail.com>
2022-10-17 14:51:15 +0700

    Sync

--- app/src/main/java/vn/tiki/TiniAppExample/MainActivity.kt
+++ app/src/main/java/vn/tiki/TiniAppExample/MainActivity.kt
@@ -2,37 +2,8 @@ package vn.tiki.TiniAppExample
 
 import android.app.Activity
 import android.os.Bundle
-import com.facebook.react.PackageList
-import com.facebook.react.ReactInstanceManager
-import com.facebook.react.ReactPackage
-import com.facebook.react.ReactRootView
-import com.facebook.react.common.LifecycleState
-import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler
-import com.facebook.soloader.SoLoader
 
-class MainActivity : Activity(), DefaultHardwareBackBtnHandler {
-    private var mReactRootView: ReactRootView? = null
-    private var mReactInstanceManager: ReactInstanceManager? = null
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        SoLoader.init(this, false)
-        mReactRootView = ReactRootView(this)
-        val packages: List<ReactPackage> = PackageList(application).getPackages()
-        mReactInstanceManager = ReactInstanceManager.builder()
-            .setApplication(application)
-            .setCurrentActivity(this)
-            .setBundleAssetName("tiniapp-runtime.jsbundle")
-            .setJSMainModulePath("index")
-            .addPackages(packages)
-            .setUseDeveloperSupport(BuildConfig.DEBUG)
-            .setInitialLifecycleState(LifecycleState.RESUMED)
-            .build()
 
-        mReactRootView!!.startReactApplication(mReactInstanceManager, "TiniAppRuntime", null)
-        setContentView(mReactRootView)
-    }
+class MainActivity : Activity() {
 
-    override fun invokeDefaultOnBackPressed() {
-        super.onBackPressed()
-    }
 }
