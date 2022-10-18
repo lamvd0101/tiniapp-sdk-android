f553dd41b631d07ea72bdb1dec8b6d7cf47ce5be
Liam Vuong <lamvd0101@gmail.com>
2022-10-18 14:00:48 +0700

    Sync

--- app/src/main/java/vn/tiki/TiniAppExample/MainApplication.java
+++ app/src/main/java/vn/tiki/TiniAppExample/MainApplication.java
@@ -1,12 +1,6 @@
 package vn.tiki.TiniAppExample;
 
 import android.app.Application;
-import android.content.Context;
-
-import com.facebook.soloader.SoLoader;
-
-import java.lang.reflect.InvocationTargetException;
-import java.util.List;
 
 public class MainApplication extends Application {
 
